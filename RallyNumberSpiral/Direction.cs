2c34c0f2578e22710574738187e9909be86770f3
Ross Dougherty <rossd987@gmail.com>
2014-02-27 13:23:24 -0700

    Create Direction.cs

---
+++ RallyNumberSpiral/Direction.cs
@@ -0,0 +1,18 @@
+
+
+public class Direction
+{
+    public static Direction CounterClockwise{
+    
+      return new Direction("CC");
+    }
+    
+    
+    public Direction(string dir)
+    {
+      
+    
+    }
+
+
+}
